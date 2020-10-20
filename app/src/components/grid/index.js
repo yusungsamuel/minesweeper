f1f543d0ffe12bfa7b4811abd1972baaec64e325
yusungsamuel <ssyyu@ucdavis.edu>
2020-10-19 20:34:02 -0700

    convert board to contain object for each square which the object contains the metadata of the square

--- app/src/components/grid/index.js
+++ app/src/components/grid/index.js
@@ -3,105 +3,147 @@ import Row from 'react-bootstrap/Row'
 import Square from "../square"
 import "./style.scss"
 const Grid = () => {
-    const [bombMap, setBombMap] = useState([
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""]
-    ])
+  const [board, setBoard] = useState([])
 
-    const [bombDirectory, setBombDirectory] = useState()
+  const createEmptyBoard = () => {
+    let data = new Array();
 
-    const calculateNeighbors = (bombDirectory, row, col) => {
-        let number = 0
-        //check top left
-        if (bombDirectory.has(`row${row - 1}column${col - 1}`)) {
-            number++
-        }
-        //check top 
-        if (bombDirectory.has(`row${row - 1}column${col}`)) {
-            number++
-        }
-        //check top right
-        if (bombDirectory.has(`row${row - 1}column${col + 1}`)) {
-            number++
-        }
-        //check left
-        if (bombDirectory.has(`row${row}column${col - 1}`)) {
-            number++
-        }
-        //check right 
-        if (bombDirectory.has(`row${row}column${col + 1}`)) {
-            number++
-        }
-        //check bottom left
-        if (bombDirectory.has(`row${row + 1}column${col - 1}`)) {
-            number++
-            //check bottom
-        }
-        if (bombDirectory.has(`row${row + 1}column${col}`)) {
-            number++
-        }
-        //check bottom right 
-        if (bombDirectory.has(`row${row + 1}column${col + 1}`)) {
-            number++
-        }
-        return number
+    for (let i = 0; i < 8; i++) {
+      data.push([]);
+      for (let j = 0; j < 8; j++) {
+        data[i][j] = {
+          x: i,
+          y: j,
+          isMine: false,
+          neighbors: 0,
+          isRevealed: false,
+          isEmpty: false,
+          isFlagged: false
+        };
+      }
+
     }
+    return data
+  }
 
-    useEffect(() => {
-        const generateIdentity = () => {
-            let bombCounts = 0
-            let temp = [...bombMap]
-            let directory = new Set()
-            while (bombCounts < 10) {
-                let row = Math.floor(Math.random() * 8);
-                let column = Math.floor(Math.random() * 8)
+  const generateBombs = data => {
+    let bombCounts = 0
+    while (bombCounts < 10) {
+      let row = Math.floor(Math.random() * 8);
+      let column = Math.floor(Math.random() * 8)
+      if (!data[row][column].isMine) {
+        data[row][column].isMine = true;
+        bombCounts++;
+      }
+    }
+    return data
+  }
 
-                if (!directory.has(`row${row}column${column}`)) {
-                    directory.add(`row${row}column${column}`)
-                    temp[row][column] = "x";
-                    bombCounts++;
-                }
-            }
-            // setBombDirectory(directory)
-            temp.forEach((row, i) => {
-                row.forEach((box, j) => {
-                    let number = calculateNeighbors(directory, i, j)
-                    if (!directory.has(`row${i}column${j}`) && number > 0) {
-                        temp[i][j] = number
-                    }
-                })
-            })
-            setBombMap(temp)
-        }
-        generateIdentity()
-    }, [])
+  const calculateNeighbors = (x, y, data) => {
+    const directions = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+    const m = data.length;
+
+    // Count the adjacent mines
+    let bombCounts = 0;
+    for (let [dx, dy] of directions) {
+      const i = x + dx, j = y + dy;
+
+      if (i >= 0 && i < m && j >= 0 && j < m && data[i][j].isMine) {
+        bombCounts++;
+      }
+    }
+    return bombCounts
+  }
+
+  const generateNewBoard = () =>{
+    let data = createEmptyBoard();
+    data = generateBombs(data);
+    data.forEach((row, x)=>{
+      row.forEach((item, y)=>{
+        item.neighbors = calculateNeighbors(x, y, data)
+      })
+    })
+    setBoard(data)
+  }
 
 
-    return (
-        <div className="container">
-            {bombMap.map((row, i) => {
-                return (
-                    <Row>
-                        {row.map((col, j) => {
-                            return (
-                                <Square
-                                    identity={col}
-                                >
-                                </Square>
-                            )
-                        })}
-                    </Row>
-                )
+  // const dfs = (x, y, board, reveal) => {
+  //   console.log(reveal)
+  //   const directions = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
+  //   const m = board.length;
+
+  //   // Count the adjacent mines
+  //   let bombCounts = 0;
+  //   for (let [dx, dy] of directions) {
+  //     const i = x + dx, j = y + dy;
+
+  //     if (i >= 0 && i < m && j >= 0 && j < m && board[i][j] === 'M') {
+  //       bombCounts++;
+  //     }
+  //   }
+
+  //   if (bombCounts > 0) {
+  //     board[x][y] = '' + bombCounts;
+  //     reveal("revealed")
+  //   }
+  //   else {
+  //     // If an empty square ('E') with no adjacent mines is revealed, then change it to revealed blank ('B')
+  //     // and all of its adjacent unrevealed squares should be revealed recursively.
+  //     // console.log("board:" + board,  "x:" + x, y)
+  //     board[x][y] = 'B';
+  //     reveal("revealed")
+
+  //     for (let [dx, dy] of directions) {
+  //       const i = x + dx, j = y + dy;
+  //       if (i >= 0 && i < m && j >= 0 && j < m && board[i][j] === "E") {
+  //         board[i][j] = 'B';
+  //         reveal("revealed")
+  //         dfs(i, j, board);
+  //       }
+  //     }
+  //   }
+  //   return board
+  // };
+
+  // const updateBoard = (coordinate, reveal, board = bombMap) => {
+  //   const [x, y] = coordinate;
+  //   if (board[x][y] === 'M') {
+  //     // If a mine ('M') is revealed, then the game is over - change it to 'X'
+  //     board[x][y] = 'X';
+  //   } else {
+  //     setBombMap(dfs(x, y, board, reveal));
+  //   }
+
+  //   setBombMap(board);
+  // };
+
+  useEffect(() => {
+    generateNewBoard();
+  }, [])
+
+  console.log(board)
+  return (
+    <div className="container">
+      {/* {bombMap.map((row, i) => {
+        return (
+          <Row>
+            {row.map((col, j) => {
+              return (
+                <Square
+                  identity={col}
+                  board={bombMap}
+                  coordinate={[i, j]}
+                  update={updateBoard}
+                >
+                </Square>
+              )
             })}
-        </div>
+          </Row>
+        )
+      })} */}
+    </div>
 
-    )
+  )
 }
 
 export default Grid;
--- app/src/components/square/index.js
+++ app/src/components/square/index.js
@@ -1,31 +1,41 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import Col from 'react-bootstrap/Col'
 import "./style.scss";
 
 const Square = (props) => {
     const [status, setStatus] = useState("covered")
-    const identity = props.identity
-    
+    // const [identity, setIdentity] = useState("")
+    const [x, y] = props.coordinate
+
     const conditionalRender = (status) => {
-        switch (status){
-            case "revealed": 
-                return identity
+        switch (status) {
+            case "revealed":
+                return props.board[x][y]
             case "flagged":
                 return "flag"
             default:
-                return " "
+                return ""
         }
     }
 
     const handleClick = (e) => {
-        if(e.type === "click"){
-            setStatus("revealed")
+        if (status === "covered") {
+            if (e.type === "click") {
+                props.update(props.coordinate, setStatus)
+            }
+            else if (e.type === 'contextmenu') {
+                e.preventDefault();
+                setStatus("flagged")
+            }
         }
-        else if(e.type === 'contextmenu'){
-            e.preventDefault();
-            setStatus("flagged")
+        else if (status === "flagged") {
+            if (e.type === 'contextmenu') {
+                e.preventDefault();
+                setStatus("covered")
+            }
         }
-    } 
+
+    }
     return (
         <Col
             onClick={handleClick}
